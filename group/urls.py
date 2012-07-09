de7e1fde4ddc76c446d6d868b07a872cb82da50c
martinhb <martin.bielecki@gmail.com>
2012-07-09 14:33:11 +0200

    Added group view

--- group/urls.py
+++ group/urls.py
@@ -1,2 +1,5 @@
 #!/usr/bin/env python2.7
-
+from django.conf.urls import patterns, include, url
+urlpatterns = patterns('',
+    url(r'^$', 'group.views.group'),
+)
--- group/views.py
+++ group/views.py
@@ -1,1 +1,12 @@
 # Create your views here.
+from group.models import Group
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
+
+def group(request):
+
+    g = Group.objects.all()
+
+    return render_to_response("groups.html", {"title": "All available groups",
+            "groups": g},
+            context_instance = RequestContext(request))
