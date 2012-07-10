b59eaa94e1d0dd289b4048feb9b4580b2c1af6b8
ilyakh <ilyakh@ulrik.uio.no>
2012-07-10 17:23:01 +0200

    final synchronization

--- prototype/urls.py
+++ prototype/urls.py
@@ -7,9 +7,7 @@ urlpatterns = patterns( '',
     url( r'tagittest$',               views.tagittest),
     url( r'intrude/([a-z]{1,10})$',   views.intrude ),
     url( r'students/$',               views.display_students ),
-    url( r'students/populate/$',      views.populate_students ),
     url( r'wishes/$',                 views.display_wishes ),
-    url( r'wishes/populate/$',        views.populate_wishes ),
     url( r'wishes/flush/$',           views.flush_wishes ),
 
     url(
