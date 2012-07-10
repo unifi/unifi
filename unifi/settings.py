8b4a66b9a6796b6fa3ae477a030d866cdd765e7b
martinhb <martin.bielecki@gmail.com>
2012-07-10 11:42:27 +0200

    sync

--- unifi/settings.py
+++ unifi/settings.py
@@ -105,7 +105,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',    
-    # 'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
@@ -124,7 +124,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.admindocs',
-    # 'debug_toolbar',
+    'debug_toolbar',
     'south',
     'crap',
     'group',
@@ -139,17 +139,17 @@ INSTALLED_APPS = (
     'communication',
 )
 
-# DEBUG_TOOLBAR_PANELS = (
-    # 'debug_toolbar.panels.version.VersionDebugPanel',
-    # 'debug_toolbar.panels.timer.TimerDebugPanel',
-    # 'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
-    # 'debug_toolbar.panels.headers.HeaderDebugPanel',
-    # 'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
-    # 'debug_toolbar.panels.template.TemplateDebugPanel',
-    # 'debug_toolbar.panels.sql.SQLDebugPanel',
-    # 'debug_toolbar.panels.signals.SignalDebugPanel',
-    # 'debug_toolbar.panels.logger.LoggingPanel',
-# )
+DEBUG_TOOLBAR_PANELS = (
+     'debug_toolbar.panels.version.VersionDebugPanel',
+     'debug_toolbar.panels.timer.TimerDebugPanel',
+     'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+     'debug_toolbar.panels.headers.HeaderDebugPanel',
+     'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+     'debug_toolbar.panels.template.TemplateDebugPanel',
+     'debug_toolbar.panels.sql.SQLDebugPanel',
+     'debug_toolbar.panels.signals.SignalDebugPanel',
+     'debug_toolbar.panels.logger.LoggingPanel',
+ )
 
 
 INTERNAL_IPS = (
