e01ec7ee3c2c8c5b1dddfcb7e8ca6e59d00203dc
ilyakh <ilyakh@ulrik.uio.no>
2012-07-12 10:53:02 +0200

    prototype sync

--- prototype/views.py
+++ prototype/views.py
@@ -1,7 +1,4 @@
-from django.contrib.auth.models import User
-from django.db.utils import IntegrityError
 from django.shortcuts import render_to_response, redirect
-from django.core.context_processors import csrf
 from django.template.context import RequestContext
 from django.views.decorators.csrf import csrf_protect
 from django.contrib import auth
@@ -83,7 +80,13 @@ def populate( request, profile=None ):
                     WishManager.addWish( wish[0], wish[1:] )
 
 
-    return redirect( "." )
+    return render_to_response( "dialog.html", {
+            "title": "The database is now populated",
+            "message": "By profile %s" % profile,
+            "set": ""
+        },
+        context_instance = RequestContext( request )
+    )
 
 
 @csrf_protect
