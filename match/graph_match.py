aa4630f9fada788afdadc4d91b59db9019be2361
martinhb <martin.bielecki@gmail.com>
2012-07-13 17:11:22 +0200

    Graph match updated

--- match/graph_match.py
+++ match/graph_match.py
@@ -6,35 +6,11 @@ import heapq as heap
 from itertools import combinations
 from rating import jaccard
 import pylab
-import pickle
 import time
 
+GROUP_SIZE = 10
+MIN_SCORE = 0.8
 
-
-
-#t0 = time.time()
-#pairs = combinations(wishes, 2)
-#g.add_nodes_from(wishes)
-
-#h = []
-#prev = None
-
-#for p in pairs:
-#    if p[0] is not prev:
-#        for i in range(4):
-#            if len(h) == 0:
-#                break # my neck
-#            hp = heap.heappop(h)
-
-#        h = []
-#        prev = p[0]
-#    w = 1 - jaccard(p[0].tags.all(), p[1].tags.all())
-#    if w < 0.5:
-#        g.add_edge(hp[1][0], hp[1][1], weight=hp[0])
-#        heap.heappush(h, (w, p))
-#
-
-GROUP_SIZE = 3
 def check_for_group(wish):
     neighbors = g.adj[wish]
     h = [] # Tha heapzor
@@ -44,16 +20,13 @@ def check_for_group(wish):
         heap.heappush(h, (n[1], n[0]))
 
 
-    # A group is found
+    # A group is found (add 1 for the current wish itself)
     if len(h) + 1 >= GROUP_SIZE:
-        print "A group is ready!"
-        print "Members:",
-        print h
-        print wish
-        print "\n"
+        return [ heap.heappop(h) for x in range(GROUP_SIZE-1)]
 
 wishes = Wish.objects.all()
 g = nx.Graph()
+t0 = time.time()
 
 for wish in wishes:
 
@@ -62,42 +35,34 @@ for wish in wishes:
     # For each existing node, calculate the score between the new wish and the node
     # 0.0 - Perfect match (all tags are the same), 1.0 - No match at all (disjoint tag sets)
     for node in g.nodes():
-        if wish.student.user.username == node.student.user.username:
+        if wish.student == node.student:
             continue
 
         score = 1 - jaccard(wish.tags.all(), node.tags.all())
 
-        if score <= 0.3:
+        if score <= MIN_SCORE:
             g.add_edge(wish, node, weight=score)
 
-    check_for_group(wish)
+    h = check_for_group(wish)
 
-#print time.time()-t0
+    if h is not None:
+        print "A group is found \n(%d) Members: " % (len(h)+1),
+        print wish, 
+        print ":", 
 
-#pos=nx.spring_layout(g, scale=10)
-#pylab.figure(10, figsize=(13,12))
-#nx.draw(g,pos, font_size=20)
-#edge_labels=dict([((u,v,),d['weight']) for u,v,d in g.edges(data=True)])
-#nx.draw_networkx_edge_labels(g,pos,edge_labels=edge_labels)
-#pylab.show()
-#
+        for item in h:
+            print item[1],
+            g.remove_node(item[1])
 
-#g = nx.minimum_spanning_tree(g)
+        g.remove_node(wish)
 
-#f = open("graphdump.dat", "wr")
-#pickle.dump(g, f)
-#f.close()
+        print "\n"
 
+print "Time: " + str(time.time()-t0) + " s"
 
-#subs = nx.connected_component_subgraphs(g)
-#pos=nx.spring_layout(g, scale=10)
-#pylab.figure(10, figsize=(10,12))
-
-#for s in subs:
-#    print sorted(s.adj)
-#    nodes = s.nodes()
-#    num = len(nodes)
-#    if num >= 2: #ant personer
-#print nodes
-
-
+pos=nx.spring_layout(g, scale=10)
+pylab.figure(10, figsize=(13,12))
+nx.draw(g,pos, font_size=20)
+edge_labels=dict([((u,v,),d['weight']) for u,v,d in g.edges(data=True)])
+nx.draw_networkx_edge_labels(g,pos,edge_labels=edge_labels)
+pylab.show()
