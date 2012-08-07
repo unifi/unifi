3bb4eda0b5f1b22dd164399a7d14162b1899e42b
ilyakh <ilyakh@ulrik.uio.no>
2012-08-07 04:09:58 +0200

    Tested out the ajax communication on the Wish objects, assigned right http-errorcodes in the view

--- commit.py
+++ commit.py
@@ -1,12 +1,14 @@
-from os import system
+from os import system, linesep
 
 branch = "master"
 
 system( "git add -u ." )
 
-issue = raw_input( "Which issue were you trying to fix? \n" )
+goals = raw_input( "What were your trying to add or fix? \n" )
 results = raw_input( "What have you achieved? \n" )
-limits = raw_input( "What limitations have you discovered? \n" )
+issues  = raw_input( "What issues have you discovered? \n" )
 
-system( "git commit -m \"%s\"" %  ("%s\n%s\n%s\n" % (issue, results, limits)) )
+system( "git commit -m \"%s\"" % \
+        linesep.join( [goals, results, issues] )
+)
 system( "git push origin %s" % branch )
