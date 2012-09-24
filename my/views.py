0cb96d4b1d8ac1284f7fe2c98d887e475b58cd6a
ilyakh <ilyakh@ulrik.uio.no>
2012-09-25 00:43:44 +0200

    a4

--- my/views.py
+++ my/views.py
@@ -79,9 +79,4 @@ class WishView( AccessRestrictedView ):
         student = UserManager.getStudent( self.user )
         wishes = Wish.objects.filter( student=student )
 
-        return render_to_response( "wish/my.html", {
-                "title": "Yaaay",
-                "wishes": wishes
-            },
-            context_instance = RequestContext( self.request )
-        )
+        return self.dialog( message="Test %s" % len(wishes) )
