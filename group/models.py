3acc63676cc8744ad67964292b689c134d88c464
unknown <ilyakh@ulrik.uio.no>
2012-10-05 01:03:01 +0200

    introduces a capacity-aware matching; extends the group models with slots and roles to allow a group to open up for a specific expertise

--- group/models.py
+++ group/models.py
@@ -3,21 +3,25 @@
 from django.db import models
 from person.models import Wish, Person
 
+DEFAULT_GROUP_CAPACITY = 5
+
 class Group(models.Model):
     """
-    Group. A person container.
+    Contains Person objects, has a flag that identifies an assistance request.
+    Number of group members is limited by its capacity, yet a group that is
+    full accepts new members if it has open slots.
     """
-
     wishes           = models.ManyToManyField( Wish )
-    persons         = models.ManyToManyField( Person, null=True )
+    persons          = models.ManyToManyField( Person, null=True )
     needs_assistance = models.BooleanField()
+    capacity         = models.IntegerField( default=DEFAULT_GROUP_CAPACITY )
+    slots            = models.ManyToManyField( Slot, null=True )
 
 
     def tags( self ):
         """
         Return group tags as a models.query.QuerySet
         """
-
         wishes = self.wishes.all()
         tags = wishes[0].tags.all()
         for w in wishes[1:]:
@@ -25,5 +29,29 @@ class Group(models.Model):
 
         return tags.distinct()
 
-    def __unicode__(self):
-        return "Group " + str(self.pk)
+    def __unicode__( self ):
+        return "Group %s" % self.pk
+
+
+
+class Slot( models.Model ):
+    """
+    Contains candidates and/or a chosen person that has applied for a slot in
+    the group. If the group has an open slot (no 'person' assigned: hence,
+    person allows null), it can accept new members even if its capacity is
+    satisfied.
+
+    A slot also identifies a 'role' of a person in the group.
+    """
+    role = models.ForeignKey( Role, null=False )
+    person = models.ForeignKey( Person, null=True )
+    candidates = models.ManyToManyField( Person, null=True )
+
+class Role( models.Model ):
+    """
+    Describes a Person's rights and properties as a member of a group or
+    the system. In other words, it can be used as an indication of what a person
+    can do and what a person needs to see.
+    """
+    name = models.CharField( max_length=50, null=False, unique=True )
+
